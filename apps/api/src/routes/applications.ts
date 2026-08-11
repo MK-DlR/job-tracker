5952fff6705dfce1c6f9c20830d4ec1646c686d4
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-08-11 12:56:50 -0400

    feat: add GET routes for listing and fetching applications

--- apps/api/src/routes/applications.ts
+++ apps/api/src/routes/applications.ts
@@ -8,11 +8,31 @@ import type { CreateApplicationInput } from "@job-tracker/shared-types";
 
 const router = Router();
 
+// create application
 router.post("/", async (req: Request<{}, {}, CreateApplicationInput>, res: Response) => {
     const application = await prisma.application.create({
         data: req.body,
     });
     res.status(201).json(application);
-})
+});
+
+// get applications
+router.get("/", async (req: Request, res: Response) => {
+    const applications = await prisma.application.findMany();
+    res.json(applications);
+});
+
+// get specific application
+router.get("/:id", async (req: Request<{id: string}>, res: Response) => {
+    const application = await prisma.application.findUnique({
+        where: { id: Number(req.params.id) },
+    });
+
+    if (!application) {
+        return res.status(404).json({ error: "Application not found" });
+    } 
+
+    res.json(application);
+});
 
 export default router;
